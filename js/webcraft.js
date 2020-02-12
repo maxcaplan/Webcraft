66107cd35a7cbec04a4d081c9fb14fab1a2a16bb
jellycream <maxacaplan@gmail.com>
2020-02-12 19:27:12 -0400

    stats.js added

--- js/webcraft.js
+++ js/webcraft.js
@@ -1,5 +1,9 @@
 console.log("Welcome to Webcraft")
 
+// Import packages
+import * as THREE from "./packages/three.module.js"
+import "./packages/simplex-noise.js"
+
 // Generate alphanumeric world seed
 const MAX_SEED_LENGTH = 19
 const SEED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456890"
@@ -12,7 +16,7 @@ for (let s = 0; s < MAX_SEED_LENGTH; s++) {
 console.log("Your seed is: " + seed)
 
 // Create instance of simplex noise using seed
-var simplex = new SimplexNoise(seed)
+let simplex = new SimplexNoise(seed)
 
 // Set default scene size
 let width = 480
@@ -22,39 +26,89 @@ let height = 270
 width = document.body.clientWidth
 height = document.body.clientHeight
 
+
+// ************** //
+// Three js setup //
+// ************** //
+
 // Three.js scene
-var scene = new THREE.Scene();
+let scene = new THREE.Scene();
 // Three.js camera (FOV, Aspect Ratio, near clipping plain, far clipping plain)
-var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
 // Setup renderer
-var renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 
 // Append renderer to body
 document.body.appendChild(renderer.domElement);
 
+// Setup skybox
+let loader = new THREE.CubeTextureLoader();
+const texture = loader.load([
+    '../resources/textures/skybox/px.png',
+    '../resources/textures/skybox/nx.png',
+    '../resources/textures/skybox/py.png',
+    '../resources/textures/skybox/ny.png',
+    '../resources/textures/skybox/pz.png',
+    '../resources/textures/skybox/nz.png',
+]);
+scene.background = texture;
+
+
 // Create simple cube:
-// Create mesh
-var geometry = new THREE.BoxGeometry();
+// Create geometry
+let geometry = new THREE.BoxGeometry(3, 3, 3);
 // Create material
-var material = new THREE.MeshBasicMaterial({
+let material = new THREE.MeshPhongMaterial({
     color: 0x00ff00
 });
-// Create cube object
-var cube = new THREE.Mesh(geometry, material);
+// Create cube mesh
+let cube = new THREE.Mesh(geometry, material);
 // Add cube to scene
 scene.add(cube);
 
+// Add light to scene
+let light = new THREE.DirectionalLight(0xFFFFFF, 1);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
 camera.position.z = 5;
 
+// FPS counter
+var stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
 
 // Render loop
-function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+function render(time) {
+    time *= 0.001; // convert time to seconds
 
-    requestAnimationFrame(animate);
+    stats.begin();
+
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+
+    // Window resize
+    let canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+
+    requestAnimationFrame(render);
     renderer.render(scene, camera);
+
+    stats.end();
 }
-animate();
+
+// ************** //
+// Game Functions //
+// ************** //
+
+// Test world gen function
+function generate() {
+
+}
+
+
+// Start render loop
+render();
