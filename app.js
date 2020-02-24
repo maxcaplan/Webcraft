b01b3bd8de16be106f0ce612fe9eec38ff57715c
jellycream <maxacaplan@gmail.com>
2020-02-24 09:54:49 -0400

    Node, npm, and express boilerplate

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, hostname, () => console.log(`Example app listening on port ${port}`))
